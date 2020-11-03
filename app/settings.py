13d21ac383d6aeb7e9a8126447ac30423084fc11
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-11-03 16:31:31 +0200

    Added template and static

--- app/settings.py
+++ app/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
 ]
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'app/static')
